962d5182eecdaa7b0f4b5059b45b1b5813f9f748
Amanda Limón <amandalimon@outlook.com>
2023-10-12 19:51:07 -0600

    Navbar component + styles

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -5,16 +5,17 @@ import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import Navbar from '../../Components/Navbar'
 import './App.css'
 
 const AppRoutes = () => {
   let routes = useRoutes([
     { path: '/', element: <Home /> },
-    { path: '/my-account', element: <MyAccount /> },
-    { path: '/my-order', element: <MyOrder /> },
-    { path: '/my-orders', element: <MyOrders /> },
-    { path: '/sign-in', element: <SignIn /> },
-    { path: '/*', element: <NotFound /> },
+    { path: '/my-account', element: <MyAccount/> },
+    { path: '/my-order', element: <MyOrder/> },
+    { path: '/my-orders', element: <MyOrders/> },
+    { path: '/sign-in', element: <SignIn/> },
+    { path: '/*', element: <NotFound/> },
   ])
 
   return routes
@@ -23,7 +24,8 @@ const AppRoutes = () => {
 const App = () => {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <AppRoutes/>
+      <Navbar/>
     </BrowserRouter>
   )
 }
