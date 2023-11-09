2a86d4d7650e558d70c530a8d436aff65918c840
Amanda Limón <amandalimon@outlook.com>
2023-11-09 17:43:28 -0600

    Searching bar and filtered titles

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react';
 
 const ShoppingCartContext = React.createContext();
 
-function ShoppingCartProvider ({children}) {
+function ShoppingCartProvider({ children }) {
 
   // Product Detail
   const [openModal, setOpenModal] = useState(false);
@@ -16,22 +17,50 @@ function ShoppingCartProvider ({children}) {
   // Shopping Cart · Order
   const [order, setOrder] = useState([]);
 
+  // Get products
+  const [items, setItems] = useState(null)
+  const [filteredItems, setFilteredItems] = useState(null)
+
+  // Get products by title
+  const [searchByTitle, setSearchByTitle] = useState(null)
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setItems(data))
+  }, [])
+
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+
+  useEffect(() => {
+    if (searchByTitle) setFilteredItems(filteredItemsByTitle(items, searchByTitle))
+  }, [items, searchByTitle])
+
+ 
+
   return (
     <ShoppingCartContext.Provider value={{
       openModal,
       setOpenModal,
       productToShow,
       setProductToShow,
-      cartProducts, 
+      cartProducts,
       setCartProducts,
       openCheckoutSM,
       setOpenCheckoutSM,
       order,
       setOrder,
+      items,
+      setItems,
+      searchByTitle,
+      setSearchByTitle,
+      filteredItems,
     }}>
       {children}
     </ShoppingCartContext.Provider>
   );
 }
 
-export {ShoppingCartContext, ShoppingCartProvider}
+export { ShoppingCartContext, ShoppingCartProvider }
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {useState, useEffect } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 import ProductDetail from '../../Components/ProductDetail'
@@ -8,33 +7,59 @@ import { ShoppingCartContext } from '../../Context'
 
 
 function Home() {
-  const { openModal } = React.useContext(ShoppingCartContext);
-  const [items, setItems] = useState (null)
+  const {
+    openModal,
+    items,
+    searchByTitle,
+    setSearchByTitle,
+    filteredItems,
+  } = React.useContext(ShoppingCartContext);
 
-  useEffect(() =>{
-    fetch('https://fakestoreapi.com/products')
-      .then(res=>res.json())
-      .then(data=> setItems(data))
-  }, [])
-
-    return (
-      <Layout>
-        Home 
-        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'> 
-        {items?.map(item => (
-        <Card 
-          key={item.id} 
-          data={item}
-        />
-        ))}
-        </div>
-        {openModal && (
-        <Modal>
-          <ProductDetail/>
-        </Modal>
-        )}
-      </Layout>
-    )
+  const renderView = () => {
+    if (searchByTitle?.length > 0) {
+      if (filteredItems?.length > 0) {
+        return (
+          filteredItems?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        )
+      } else {
+        return (
+          <div>We don't have anything</div>
+        )
+      }
+    } else {
+      return (
+        items?.map(item => (
+          <Card key={item.id} data={item} />
+        ))
+      )
+    }
   }
-  
-  export default Home 
+
+  return (
+    <Layout>
+
+      <h1 className='mt-3 font-medium text-2xl'>Exclusive Products</h1>
+
+      <input
+        type="text"
+        placeholder='Looking for a product?'
+        className='rounded-lg border-black w-1/3 p-4 mb-4 focus:outline-none'
+        onChange={(event) => setSearchByTitle(event.target.value)}
+      />
+      
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {renderView()}
+      </div>
+
+      {openModal && (
+        <Modal>
+          <ProductDetail />
+        </Modal>
+      )}
+    </Layout >
+  )
+}
+
+export default Home 
